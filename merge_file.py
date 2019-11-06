afca96ca33b52dbd51900cd98a5b5ccbedd0f313
Alfred.W <yafei.wangyf@alibaba-inc.com>
2019-11-06 16:15:11 +0300

    anything ...

---
+++ merge_file.py
@@ -0,0 +1,31 @@
+
+
+
+
+import os.path
+import time
+time1=time.time()
+from lib.utility.path import *
+
+
+##########################合并同一个文件夹下多个txt################
+def MergeTxt(filepath,outfile):
+    k = open(filepath+outfile, 'a+')
+    for parent, dirnames, filenames in os.walk(filepath):
+        for filepath in filenames:
+            txtPath = os.path.join(parent, filepath)  # txtpath就是所有文件夹的路径
+            f = open(txtPath)
+            ##########换行写入##################
+            k.write(f.read()+"\n")
+
+    k.close()
+
+    print("finished")
+
+
+if __name__ == '__main__':
+    filepath = DATA_PATH + "/ke/xiaoqu/hz/20190930/"
+    outfile = "result.txt"
+    MergeTxt(filepath,outfile)
+    time2 = time.time()
+    print(u'总共耗时：' + str(time2 - time1) + 's')
